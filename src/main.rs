99cdb612386064d8d5a8ce17a458730963142f9e
DaringCuteSeal <daringcuteseal@gmail.com>
2024-12-31 15:00:50 +0700

    fix: add the log_file field to the struct construction

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod cli;
 
 fn main() -> AppResult<AppResultData> {
     let args = cli::CLIArgs::parse_check()?;
-    println!("{:?}", args.file_a);
-    println!("{:?}", args.file_b);
-    Ok(AppResultData {successful_tests: 100, failed_tests: 0})
+    println!("{:?}", args.executable_a);
+    println!("{:?}", args.executable_b);
+    Ok(AppResultData {successful_tests: 100, failed_tests: 0, log_file: None})
 }
