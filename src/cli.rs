7361669270c302b55f14f43338e3f4ac93b3f2ef
DaringCuteSeal <daringcuteseal@gmail.com>
2024-12-31 14:00:48 +0700

    feat: turn input and output seperators as options

--- src/cli.rs
+++ src/cli.rs
@@ -14,13 +14,21 @@ pub(crate) struct CLIArgs {
 
     /// Path to the second executable
     #[arg()]
-    pub(crate) file_b: PathBuf
+    pub(crate) file_b: PathBuf,
+
+    /// Input separator
+    #[arg(short = 's', default_value = " ")]
+    pub(crate) input_sep: char,
+
+    /// Output separator
+    #[arg(short = 'o', default_value = " ")]
+    pub(crate) ouput_sep: char,
 }
 
 impl CLIArgs {
     /// A wrapper function around the `Self::Parse` method. This method returns an
     /// `AppResult<Self>` containing an app error when an argument parsing error occured.
-    pub fn parse_all() -> AppResult<Self> {
+    pub fn parse_check() -> AppResult<Self> {
         let result = Self::parse();
         if !result.file_a.is_file() {
             return Err(AppError::FileNotFound(result.file_a))
