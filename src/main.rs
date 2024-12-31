e1b47d1f3d36257e523f85be57281de34f688081
DaringCuteSeal <daringcuteseal@gmail.com>
2024-12-31 14:45:45 +0700

    refactor: rename parse_all to parse_check

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod error;
 mod cli;
 
 fn main() -> AppResult<AppResultData> {
-    let args = cli::CLIArgs::parse_all()?;
+    let args = cli::CLIArgs::parse_check()?;
     println!("{:?}", args.file_a);
     println!("{:?}", args.file_b);
     Ok(AppResultData {successful_tests: 100, failed_tests: 0})
